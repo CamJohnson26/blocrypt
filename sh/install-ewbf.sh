d22aeee8a5df7e6bc915fe6b18da89467d1d56c1
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2018-01-06 20:19:49 -0600

    Fixed ewbf

--- sh/install-ewbf.sh
+++ sh/install-ewbf.sh
@@ -4,4 +4,5 @@
 mkdir ~/Desktop/miners
 mkdir ~/Desktop/miners/ewbf
 wget https://github.com/nanopool/ewbf-miner/releases/download/v0.3.4b/Zec.miner.0.3.4b.Linux.Bin.tar.gz ~/Desktop/miners/ewbf
-
+gzip -d ~/Desktop/miners/ewbf/Zec.miner.0.3.4b.Linux.Bin.tar.gz
+tar -xvf ~/Desktop/miners/ewbf/Zec.miner.0.3.4b.Linux.Bin.tar
