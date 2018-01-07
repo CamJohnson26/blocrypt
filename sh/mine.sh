53f416bc50b7dd7b39031c620603c8feb11a1217
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2018-01-06 19:53:14 -0600

    swapped cp files

--- sh/mine.sh
+++ sh/mine.sh
@@ -4,5 +4,5 @@
 # May want to migrate nohup to screen
 
 sudo ./stop-mine.sh
-sudo cp ~/mining.log "~/archive/mining-$(date).log" > /dev/null
+sudo cp "~/archive/mining-$(date).log" ~/mining.log > /dev/null
 nohup sudo ~/Desktop/mine-$1.sh > ~/mining.log 2>&1 &
