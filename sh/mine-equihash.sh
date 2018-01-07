b91fc339696960a621ab128eed7a677ef87965cd
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2018-01-06 20:13:27 -0600

    fixed the equihash miner to point at local user file system

--- sh/mine-equihash.sh
+++ sh/mine-equihash.sh
@@ -9,4 +9,4 @@ sudo nvidia-smi -pm 1
 sudo nvidia-smi -pl 70
 sudo ./overclocks/$HOSTNAME.sh
 
-~/Desktop/miners/ewbf/miner --server $1 --user $2.$HOSTNAME --pass $3 --port $4
+/home/$USER/Desktop/miners/ewbf/miner --server $1 --user $2.$HOSTNAME --pass $3 --port $4
