052b3eb5fab0171197fbafe9664e51ba7870ebe8
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2018-01-06 19:12:34 -0600

    Overclocks and equihash
    Added overclocks and equihash scripts

---
+++ sh/mine-equihash.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+# Parameters
+# $1: pool url
+# $2: username
+# $3: password
+# $4: port
+
+sudo nvidia-smi -pm 1
+sudo nvidia-smi -pl 70
+sudo ./overclocks/$HOSTNAME
+
+~/Desktop/miners/ewbf/miner --server $1 --user $2.$HOSTNAME --pass $3 --port $4
