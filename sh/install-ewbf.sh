fce9338a5105a142eefe4213c8df637a76e94dc8
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2018-01-06 20:54:34 -0600

    tar command was wrong

--- sh/install-ewbf.sh
+++ sh/install-ewbf.sh
@@ -5,4 +5,5 @@ mkdir ~/Desktop/miners
 mkdir ~/Desktop/miners/ewbf
 wget -P ~/Desktop/miners/ewbf https://github.com/nanopool/ewbf-miner/releases/download/v0.3.4b/Zec.miner.0.3.4b.Linux.Bin.tar.gz
 gzip -d ~/Desktop/miners/ewbf/Zec.miner.0.3.4b.Linux.Bin.tar.gz
-tar -xvf -C ~/Desktop/miners/ewbf  ~/Desktop/miners/ewbf/Zec.miner.0.3.4b.Linux.Bin.tar
+tar -C ~/Desktop/miners/ewbf -xvf  ~/Desktop/miners/ewbf/Zec.miner.0.3.4b.Linux.Bin.tar
+rm ~/Desktop/miners/ewbf/Zec.miner.0.3.4b.Linux.Bin.tar
