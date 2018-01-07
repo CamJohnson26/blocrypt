61fde04a40976d4eaadbb1ef5867050078aa5441
Mostlyharmless26 <Cameronrossjohnson@gmail.com>
2018-01-06 19:49:05 -0600

    Removed %T cause it doesn't work and don't need it

--- sh/mine.sh
+++ sh/mine.sh
@@ -4,5 +4,5 @@
 # May want to migrate nohup to screen
 
 sudo ./stop-mine.sh
-sudo cp ~/mining.log ~/archive/mining$(date + %T).log
+sudo cp ~/mining.log ~/archive/mining-$(date).log
 nohup sudo ~/Desktop/mine-$1.sh > ~/mining.log 2>&1 &
